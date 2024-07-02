959132bfb35f9ecaf184484ea161f218a78cea64
goosebbeoms <seo980916@gmail.com>
2024-07-02 22:40:39 +0900

    181. useEffect를 활용하는 다른 적용 사례

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const storedPlaces = storedIds.map((id) => {
 })
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   const [availablePlaces, setAvailablePlaces] = useState([])
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -33,12 +33,12 @@ function App() {
 
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true)
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false)
   }
 
   function handleSelectPlace(id) {
@@ -69,7 +69,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
