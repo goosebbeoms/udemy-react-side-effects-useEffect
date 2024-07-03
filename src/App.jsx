fb65321f187849fec96b75829058938d0b5ed68c
goosebbeoms <seo980916@gmail.com>
2024-07-04 02:53:30 +0900

    188. useCallback 훅

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -51,7 +51,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) => prevPickedPlaces.filter((place) => place.id !== selectedPlace.current));
     setModalIsOpen(false)
 
@@ -62,7 +62,8 @@ function App() {
         id !== selectedPlace.current;
       })
     );
-  }
+  }, [])
+  
 
   return (
     <>
